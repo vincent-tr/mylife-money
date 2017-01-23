3bac60d6e3631c35afd9896023b60800bb09cd21
Vincent Trumpff <vincent@stoic.com>
2017-01-23 15:28:04 +0100

    webpack/babel/react basic setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR   = path.resolve(__dirname, 'public/app');
+
+const config = {
+  entry: APP_DIR + '/main.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
