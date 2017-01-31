fc2bf85cbc7fc3609c02401f7c34abe374eaa996
Vincent Trumpff <vincent@stoic.com>
2017-01-31 10:33:38 +0100

    groups reducer

--- public/app/reducers/groups.js
+++ public/app/reducers/groups.js
@@ -1,5 +1,26 @@
 'use strict';
 
-export default function(state = {}, action) {
-  return state;
-}
+import { handleActions } from 'redux-actions';
+import { actionTypes } from '../constants/index';
+import Immutable from 'immutable';
+
+export default handleActions({
+
+  [actionTypes.GET_GROUPS] : {
+    next : (state, action) => state.withMutations((map => {
+      map.clear();
+
+      map.set(null, {
+        id      : null,
+        display : 'Non triés'
+      });
+
+      for(const raw of action.payload) {
+        const { _id: id, ...props } = raw;
+        const group = Object.assign({ id }, props);
+        map.set(id, group);
+      }
+    }))
+  }
+
+}, Immutable.Map());
