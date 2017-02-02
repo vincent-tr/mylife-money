42e86061d8c0fa2771e8e1555bfd2196146adc73
Vincent Trumpff <vincent@stoic.com>
2017-02-02 14:52:54 +0100

    properly display nested items

--- public/app/components/groups/node.js
+++ public/app/components/groups/node.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import base from '../base/index';
 
+import GroupNodeContainer from '../../containers/groups/node-container';
+
 const GroupNode = ({ group, children }) => (
   <base.SelectableListItem
     value={{ id: group.id }}
     primaryText={group.display}
-    nestedItems={children} />
+    nestedItems={children.map((child) => (<GroupNodeContainer key={child.id} group={child} />))} />
 );
 
 GroupNode.propTypes = {
