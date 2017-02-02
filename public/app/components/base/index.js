2ea1c2852985f1b3ce73acef06daaa932c5f78db
Vincent Trumpff <vincent@stoic.com>
2017-02-02 21:10:39 +0100

    remove selectable list

--- public/app/components/base/index.js
+++ public/app/components/base/index.js
@@ -1,13 +1,9 @@
 'use strict';
 
-import SelectableList from './selectable-list';
-import SelectableListItem from './selectable-list-item';
 import TooltipContainer from './tooltip-container';
 import TabTemplate from './tab-template';
 
 export default {
-  SelectableList,
-  SelectableListItem,
   TooltipContainer,
   TabTemplate
 };
