20ca5a917a858b799d1b3756b7f34caa0335a0d4
Vincent Trumpff <vincent@stoic.com>
2017-02-03 18:46:33 +0100

    webpack configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,14 +22,7 @@ const config = {
       }
     ]
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+  devtool: 'eval'
 };
 
 module.exports = config;
