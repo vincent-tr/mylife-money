974628d7be3837120540ffca731b5171af048bc2
Vincent Trumpff <vincent@stoic.com>
2017-02-08 11:28:02 +0100

    cleanup

--- public/app/components/management/table.js
+++ public/app/components/management/table.js
@@ -35,7 +35,7 @@ function rowSelectionPayload(tableSelection, operations) {
   return { operations: tableSelection.map(index => operations[index].operation.id) };
 }
 
-const Table = ({ onSelect, operations }) => {
+function summaries(operations) {
   let totalDebit = 0;
   let totalCredit = 0;
   let total = 0;
@@ -47,6 +47,14 @@ const Table = ({ onSelect, operations }) => {
   totalDebit = Math.round(totalDebit * 100) / 100;
   totalCredit = Math.round(totalCredit * 100) / 100;
   total = Math.round(total * 100) / 100;
+
+  return {
+    totalDebit, totalCredit, total
+  };
+}
+
+const Table = ({ onSelect, operations }) => {
+  const { totalDebit, totalCredit, total } = summaries(operations);
   return (
     <div style={tabStyles.fullHeight}>
       <mui.Table style={tabStyles.fullHeight} wrapperStyle={styles.tableWrapper} multiSelectable={true} onRowSelection={(val) => onSelect(rowSelectionPayload(val, operations))}>
