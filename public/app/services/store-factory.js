08794644c12519242ce0d68facb28324dc632ba4
Vincent Trumpff <vincent@stoic.com>
2017-04-11 09:21:18 +0200

    remove immutableStateInvariant

--- public/app/services/store-factory.js
+++ public/app/services/store-factory.js
@@ -3,14 +3,14 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
-import immutableStateInvariant from 'redux-immutable-state-invariant'; // FIXME: remove immutableStateInvariant in production
+//import immutableStateInvariant from 'redux-immutable-state-invariant'; // FIXME: remove immutableStateInvariant in production
 
 import dataService from './data-service';
 import reducer from '../reducers/index';
 
 const store = createStore(
   reducer,
-  applyMiddleware(dataService, immutableStateInvariant(), thunk, createLogger())
+  applyMiddleware(dataService, /*immutableStateInvariant(),*/ thunk, createLogger())
 );
 
 export default store;
