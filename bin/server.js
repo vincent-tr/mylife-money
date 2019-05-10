5d3efbae1297317994b09bcdb5b8e05631855778
vincent-tr <vincent.tr@gmail.com>
2019-05-10 21:19:45 +0200

    try to deal with init

--- bin/server.js
+++ bin/server.js
@@ -1,19 +1,6 @@
 'use strict';
 
-const path = require('path');
-const { init } = require('mylife-tools-server');
+require('../lib/init');
+const { runWebServer } = require('mylife-tools-server');
 
-init({
-  baseDirectory: path.resolve(__dirname, '..'),
-  applicationName: 'mylife-money'
-});
-
-const Server = require('../lib/server');
-const server = new Server();
-
-function terminate() {
-  server.close(() => process.exit());
-}
-
-process.on('SIGINT', terminate);
-process.on('SIGTERM', terminate);
+runWebServer();
