e148cbef70c28616267e9190442ddf4084ad6f94
vincent-tr <vincent.tr@gmail.com>
2019-05-11 10:28:21 +0200

    wip web server

--- bin/server.js
+++ bin/server.js
@@ -2,5 +2,7 @@
 
 require('../lib/init');
 const { runWebServer } = require('mylife-tools-server');
+const webpackConfig = require('../webpack.config.dev.js');
+const createApi = require('../lib/api');
 
-runWebServer();
+runWebServer({ webpackConfig, apiCreator : app => { app.use('/api', createApi()); } });
