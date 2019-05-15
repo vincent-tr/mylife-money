1ea24c7d44d61e80474dab8fe4e3bccdf29a36c8
vincent-tr <vincent.tr@gmail.com>
2019-05-15 14:05:41 +0200

    wip rewrite business

--- bin/server.js
+++ bin/server.js
@@ -4,6 +4,6 @@
 
 require('../lib/init');
 const { runWebServer } = require('mylife-tools-server');
-const createApi = require('../lib/api');
+const { createApi } = require('../lib/api');
 
-runWebServer({ apiCreator : app => { app.use('/api', createApi()); } });
+runWebServer({ apiCreator: createApi });
