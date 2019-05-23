aad7c9c9fb543b79b10c16c198d7e0aba39174d9
vincent-tr <vincent.tr@gmail.com>
2019-05-23 21:44:22 +0200

    adapt new

--- public/src/services/store-factory.js
+++ public/src/services/store-factory.js
@@ -2,8 +2,7 @@
 
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-//import immutableStateInvariant from 'redux-immutable-state-invariant'; // FIXME: remove immutableStateInvariant in production
+import { createLogger } from 'redux-logger';
 
 import dataService from './data-service';
 import reducer from '../reducers/index';
