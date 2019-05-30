32bd4b21a7cc32b2879dcaf6d914dc64a1be4dcb
vincent-tr <vincent@stoic.com>
2019-05-30 17:25:03 +0200

    integrate mylife-tools-ui

--- public/src/main.js
+++ public/src/main.js
@@ -3,20 +3,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'typeface-roboto';
-import 'material-icons/iconfont/material-icons.css';
-
 import { getAccounts, getGroups } from './actions/common';
 import { getOperations } from './actions/management';
 import Application from './components/application';
 
 import store from './services/store-factory';
 
+import { services } from 'mylife-tools-ui';
+import dataService from './services/data-service';
+import reducer from './reducers/index';
+
+services.initStore(reducer, dataService);
+
+services.render({ routes: (
+  <div />
+)});
+
 store.dispatch(getAccounts());
 store.dispatch(getGroups());
 store.dispatch(getOperations());
-
-ReactDOM.render(
-  <Application/>,
-  document.getElementById('content')
-);
