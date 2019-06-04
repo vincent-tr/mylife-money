dbd3a91f097adc969deabd4ddad7fc0bf045a6ff
vincent-tr <vincent@stoic.com>
2019-06-04 12:08:17 +0200

    try to fix layout

--- public/src/components/management/index.js
+++ public/src/components/management/index.js
@@ -1,50 +1,43 @@
 'use strict';
 
-import { React, mui, clsx } from 'mylife-tools-ui';
+import { React, mui } from 'mylife-tools-ui';
 
 import GroupTree from './group-tree';
 import Toolbar from './toolbar';
 import Header from './header';
 import Table from './table';
 
-import tabStyles from '../base/tab-styles';
-
 const { makeStyles } = mui;
 
 const useStyles = makeStyles(() => ({
   div: {
     display: 'flex',
-    flexDirection: 'row wrap',
+    flexDirection: 'row',
   },
-  paperLeft: {
+  tree: {
     flex: 1,
-    height: '100%',
     textAlign: 'center',
+    overflow: 'auto',
   },
-  paperRight: {
+  table: {
     flex: 5,
-    height: '100%',
     textAlign: 'center',
-  },
-  ...tabStyles
+    overflow: 'auto',
+  }
 }));
 
 const Management = () => {
   const classes = useStyles();
   return (
     <div className={classes.div}>
-      <mui.Paper className={clsx(classes.paperLeft, tabStyles.fullHeight)}>
-        <div className={classes.fullHeight}>
-          <GroupTree />
-          <Toolbar />
-        </div>
-      </mui.Paper>
-      <mui.Paper className={clsx(classes.paperRight, tabStyles.fullHeight)}>
-        <div className={classes.fullHeight}>
-          <Header />
-          <Table />
-        </div>
-      </mui.Paper>
+      <mui.Container className={classes.tree}>
+        <GroupTree />
+        <Toolbar />
+      </mui.Container>
+      <mui.Container className={classes.table}>
+        <Header />
+        <Table />
+      </mui.Container>
     </div>
   );
 };
