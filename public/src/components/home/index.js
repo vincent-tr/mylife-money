c8c211652d833f474efe455f46719218c4f7e9ab
vincent-tr <vincent@stoic.com>
2019-06-05 17:10:17 +0200

    wip home tests

---
+++ public/src/components/home/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import { React, useMemo, mui, useDispatch, dialogs } from 'mylife-tools-ui';
+
+const useConnect = () => {
+  const dispatch = useDispatch();
+  return useMemo(() => ({
+    busySet : (value) => dispatch(dialogs.busySet(value)),
+    notificationShow : (type) => dispatch(dialogs.notificationShow({ message: type, header: 'header', type : dialogs.notificationShow.types[type] })),
+  }), [dispatch]);
+};
+
+const Home = () => {
+  const { busySet, notificationShow } = useConnect();
+
+  const onBusy = () => {
+    busySet(true);
+    setTimeout(() => busySet(false), 3000);
+  };
+
+  const onError = () => {
+    busySet(new Error('error toto'));
+  };
+
+  return (
+    <div>
+      <mui.Button onClick={onBusy}>Busy</mui.Button>
+      <mui.Button onClick={onError}>Error</mui.Button>
+      <mui.Button onClick={() => notificationShow('info')}>Notification Info</mui.Button>
+      <mui.Button onClick={() => notificationShow('warning')}>Notification Warning</mui.Button>
+      <mui.Button onClick={() => notificationShow('error')}>Notification Error</mui.Button>
+      <mui.Button>Custom dialog</mui.Button>
+    </div>
+  );
+};
+
+export default Home;
--- public/src/main.js
+++ public/src/main.js
@@ -8,13 +8,14 @@ import { getAccounts, getGroups } from './actions/common';
 import { getOperations } from './actions/management';
 
 import icons from './components/icons';
+import Home from './components/home';
 import Management from './components/management';
 // import Reporting from './containers/reporting/index-container';
 
 services.initStore(reducers, dataService);
 
 const routes = [
-  { location: '/', renderer: () => 'Home' },
+  { location: '/', renderer: () => <Home /> },
   { location: '/management', name: 'Gestion', icon: icons.tabs.Management, renderer: () => <Management /> },
   { location: '/reports/1', name: 'Rapport 1', icon: icons.tabs.Reporting, renderer: () =>'Report1' },
   { location: '/reports/2', name: 'Rapport 2', icon: icons.tabs.Reporting, renderer: () => 'Report2' },
