25d42fa10c08633f74bbbdb274595235abdcd2d2
vincent-tr <vincent@stoic.com>
2019-06-06 11:01:53 +0200

    cleanup

--- public/src/components/base/index.js
+++ public/src/components/base/index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-import TabTemplate from './tab-template';
 import IconFileButton from './icon-file-button';
 import confirm from './confirm-dialog';
 import input from './input-dialog';
 
 export default {
-  TabTemplate,
   IconFileButton,
   confirm,
   input
