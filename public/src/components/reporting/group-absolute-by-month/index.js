f8dd1d22b35c51aafca0c0eeb5c84d126051108a
vincent-tr <vincent.tr@gmail.com>
2019-06-09 10:12:46 +0200

    move + cleanup

--- public/src/components/reporting/group-absolute-by-month/index.js
+++ public/src/components/reporting/group-absolute-by-month/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 import { React, useState, useMemo, PropTypes, mui,  chart, ToolbarFieldTitle, ToolbarSeparator, useDispatch, useSelector } from 'mylife-tools-ui';
-import icons from '../icons';
-import tabStyles from '../base/tab-styles';
-import { getOperations } from '../../selectors/reporting';
-import { getGroups, makeGetGroupBags, makeGetGroupStacks } from '../../selectors/groups';
-import { refreshOperations } from '../../actions/reporting';
+import icons from '../../icons';
+import tabStyles from '../../base/tab-styles';
+import { getOperations } from '../../../selectors/reporting';
+import { getGroups, makeGetGroupBags, makeGetGroupStacks } from '../../../selectors/groups';
+import { refreshOperations } from '../../../actions/reporting';
 
-import AccountSelector from '../common/account-selector';
-import GroupSelector from '../common/group-selector';
+import AccountSelector from '../../common/account-selector';
+import GroupSelector from '../../common/group-selector';
 
 const useConnect = () => {
   const dispatch = useDispatch();
@@ -141,6 +141,39 @@ Toolbar.propTypes = {
   onCriteriaChanged: PropTypes.func.isRequired
 };
 
+const Chart = ({ data, groups, groupStacks }) => {
+
+  // http://materialuicolors.co/
+  const colors = [
+    '#EF9A9A', '#90CAF9', '#C5E1A5', '#FFAB91',
+    '#F48FB1', '#81D4FA', '#E6EE9C', '#BCAAA4'
+  ];
+
+  if(!data.length) { return null; }
+
+  const series = groups.map((group, index) => ({
+    index,
+    group,
+    display : groupStacks.get(group).map(group => group.display).join('/'),
+    fill    : colors[index % colors.length]
+  }));
+
+  return (
+    <div style={styles.chartWrapper}>
+      <chart.ResponsiveContainer>
+        <chart.BarChart data={data} margin={{top: 20, right: 20, left: 20, bottom: 20}}>
+          <chart.XAxis dataKey="date" name="Date" />
+          <chart.YAxis name="Montant" />
+          <chart.CartesianGrid strokeDasharray="3 3"/>
+          <chart.Tooltip/>
+          <chart.Legend />
+          {series.map(serie => (<chart.Bar key={serie.index} dataKey={`group-${serie.group}`} name={serie.display} fill={serie.fill} />))}
+        </chart.BarChart>
+      </chart.ResponsiveContainer>
+    </div>
+  );
+};
+
 class GroupAbsoluteByMonth extends React.Component {
 
   constructor(props) {
@@ -211,47 +244,15 @@ class GroupAbsoluteByMonth extends React.Component {
     this.setState({ data });
   }
 
-  renderReport() {
-    const { groupStacks } = this.props;
+  render() {
     const { criteria, data } = this.state;
     const { groups } = criteria;
+    const { groupStacks } = this.props;
 
-    // http://materialuicolors.co/
-    const colors = [
-      '#EF9A9A', '#90CAF9', '#C5E1A5', '#FFAB91',
-      '#F48FB1', '#81D4FA', '#E6EE9C', '#BCAAA4'
-    ];
-
-    if(!data.length) { return null; }
-
-    const series = groups.map((group, index) => ({
-      index,
-      group,
-      display : groupStacks.get(group).map(group => group.display).join('/'),
-      fill    : colors[index % colors.length]
-    }));
-
-    return (
-      <div style={styles.chartWrapper}>
-        <chart.ResponsiveContainer>
-          <chart.BarChart data={data} margin={{top: 20, right: 20, left: 20, bottom: 20}}>
-            <chart.XAxis dataKey="date" name="Date" />
-            <chart.YAxis name="Montant" />
-            <chart.CartesianGrid strokeDasharray="3 3"/>
-            <chart.Tooltip/>
-            <chart.Legend />
-            {series.map(serie => (<chart.Bar key={serie.index} dataKey={`group-${serie.group}`} name={serie.display} fill={serie.fill} />))}
-          </chart.BarChart>
-        </chart.ResponsiveContainer>
-      </div>
-    );
-  }
-
-  render() {
     return (
       <div style={tabStyles.fullHeight}>
         <Toolbar onCriteriaChanged={(criteria) => this.changeCriteria(criteria)} />
-        {this.renderReport()}
+        <Chart data={data} groups={groups} groupStacks={groupStacks} />
       </div>
     );
   }
