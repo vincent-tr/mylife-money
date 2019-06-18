f6af84005e86bc66104cc94ace49411f74b1cb87
vincent-tr <vincent@stoic.com>
2019-06-18 09:57:28 +0200

    naming

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@
 
 require('../lib/init');
 const { runServices, WebServer, IO, TaskQueue, ApiRegistry, Database } = require('mylife-tools-server');
-const { createApi } = require('../lib/web-api');
+const { webApiFactory } = require('../lib/web-api');
 const { apiServices } = require('../lib/api');
 
-runServices({ services: [WebServer, IO, TaskQueue, ApiRegistry, Database], apiCreator: createApi, apiServices });
+runServices({ services: [WebServer, IO, TaskQueue, ApiRegistry, Database], webApiFactory, apiServices });
