1d4c73cad3b231c82986652015668f98e2ef2662
vincent-tr <vincent@stoic.com>
2019-06-18 09:46:16 +0200

    api

--- bin/server.js
+++ bin/server.js
@@ -5,5 +5,6 @@
 require('../lib/init');
 const { runServices, WebServer, IO, TaskQueue, ApiRegistry, Database } = require('mylife-tools-server');
 const { createApi } = require('../lib/web-api');
+const { apiServices } = require('../lib/api');
 
-runServices({ services: [WebServer, IO, TaskQueue, ApiRegistry, Database], apiCreator: createApi });
+runServices({ services: [WebServer, IO, TaskQueue, ApiRegistry, Database], apiCreator: createApi, apiServices });
