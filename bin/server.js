633dacba38caf5ec7e9d4303a2a617a5cd510e25
vincent-tr <vincent@stoic.com>
2019-07-29 17:53:44 +0200

    store configuration

--- bin/server.js
+++ bin/server.js
@@ -6,7 +6,8 @@ require('../lib/init');
 const { runServices } = require('mylife-tools-server');
 const { apiServices } = require('../lib/api');
 const metadataDefintions = require('../shared/metadata');
+const storeConfiguration = require('../lib/store-configuration');
 
 const services = ['web-server', 'store'];
-const parameters = { apiServices, metadataDefintions };
+const parameters = { apiServices, metadataDefintions, storeConfiguration };
 runServices({ services, ... parameters });
