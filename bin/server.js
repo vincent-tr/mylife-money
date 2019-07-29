2f01dbdb04652d504e1b11d4b3bbb303c4ffe9e0
vincent-tr <vincent@stoic.com>
2019-07-29 15:47:24 +0200

    register metadata

--- bin/server.js
+++ bin/server.js
@@ -5,5 +5,8 @@
 require('../lib/init');
 const { runServices } = require('mylife-tools-server');
 const { apiServices } = require('../lib/api');
+const metadataDefintions = require('../shared/metadata');
 
-runServices({ services: ['web-server', 'database'], apiServices });
+const services = ['web-server', 'database', 'metadata-manager'];
+const parameters = { apiServices, metadataDefintions };
+runServices({ services, ... parameters });
---
+++ shared/metadata/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.datatypes = [];
+exports.entities = [
+  require('./entities/base'),
+  require('./entities/account'),
+  require('./entities/group'),
+  require('./entities/operation')
+];
