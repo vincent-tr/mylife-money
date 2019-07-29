f1a0d5aa02a4d426e4b36e3f41602755568e8087
vincent-tr <vincent@stoic.com>
2019-07-29 12:08:38 +0200

    group metadata

--- shared/metadata/entities/group.js
+++ shared/metadata/entities/group.js
@@ -1,1 +1,13 @@
 'use strict';
+
+module.exports = {
+  id: 'group',
+  parent: 'base',
+  name: 'Groupe',
+  fields: [
+    { id: 'parent', name: 'Groupe parent', datatype: 'group' },
+    { id: 'display', name: 'Affichage', datatype: 'name', constraints: ['not-null', 'not-empty'] },
+    { id: 'rules', name: 'Règles', datatype: 'any' }
+  ],
+  display: obj => obj.display
+};
