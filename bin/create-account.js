e27493f99972ca2357101fb5f13ff3f5eea202ca
vincent-tr <vincent@stoic.com>
2019-07-30 17:25:24 +0200

    config

--- bin/create-account.js
+++ bin/create-account.js
@@ -5,16 +5,18 @@
 require('../lib/init');
 const { createLogger, runTask } = require('mylife-tools-server');
 const business = require('../lib/business');
+const metadataDefintions = require('../shared/metadata');
+const storeConfiguration = require('../lib/store-configuration');
 
 const logger = createLogger('mylife:money:create-account');
 
-runTask({ services: ['database'], task: async () => {
+runTask({ services: ['store'], metadataDefintions, storeConfiguration, task: async () => {
   // TODO: rewrite with yargs
   const code    = process.argv[2];
   const display = process.argv[3];
 
   if(!code || !display) {
-    console.log('Usage: bin/create-account.js <code> <display>');
+    console.log('Usage: bin/create-account.js <code> <display>'); // eslint-disable-line no-console
     return;
   }
 
