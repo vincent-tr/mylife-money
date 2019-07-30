2f9587c8987b24a05d906aff2a6a7619bcdad3f1
vincent-tr <vincent@stoic.com>
2019-07-30 08:03:36 +0200

    move base into builtins

--- shared/metadata/entities/base.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  id: 'base',
-  fields: [
-    { id: '_id', datatype: 'identifier', constraints: ['hidden', 'readonly', 'not-null'] }
-  ]
-};
--- shared/metadata/index.js
+++ shared/metadata/index.js
@@ -2,7 +2,6 @@
 
 exports.datatypes = [];
 exports.entities = [
-  require('./entities/base'),
   require('./entities/account'),
   require('./entities/group'),
   require('./entities/operation')
