37698fff90a721a10d74437248d2b6681dd64479
vincent-tr <vincent@stoic.com>
2019-08-15 10:37:52 +0200

    change report result layout

--- shared/metadata/entities/report-group-by-month.js
+++ shared/metadata/entities/report-group-by-month.js
@@ -4,9 +4,7 @@ module.exports = {
   id: 'report-group-by-month',
   parent: 'base',
   fields: [
-    { id: 'month', name: 'Mois', datatype: 'name' },
-    { id: 'group', name: 'Groupe', datatype: 'group' },
-    { id: 'stack', name: 'Pile du graphique', datatype: 'name' },
-    { id: 'amount', name: 'Montant', datatype: 'amount' },
+    { id: 'month', datatype: 'name' },
+    { id: 'groups', datatype: 'any' },
   ]
 };
