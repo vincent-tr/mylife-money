b959527c0c014a15b50fbdbcae5dfdd056b5b797
vincent-tr <vincent@stoic.com>
2019-08-27 10:07:07 +0200

    renotify report

--- public/src/actions/reporting.js
+++ public/src/actions/reporting.js
@@ -11,16 +11,23 @@ const local = {
 export const getGroupByMonth = (criteria) => async (dispatch, getState) => {
   const state = getState();
 
+  const viewId = getViewId(state);
+  if(viewId) {
+    await dispatch(io.call({
+      service: 'reporting',
+      method: 'renotifyGroupByMonth',
+      viewId,
+      ... criteria
+    }));
+
+    return;
+  }
+
   const newViewId = await dispatch(io.call({
     service: 'reporting',
     method: 'notifyGroupByMonth',
     ... criteria
   }));
-
-  const oldViewId = getViewId(state);
-  if(oldViewId) {
-    await dispatch(io.unnotify(oldViewId));
-  }
 
   dispatch(local.setView(newViewId));
 };
